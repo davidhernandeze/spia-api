b4ba6a0c54f1f6b2869862798f9a9a30736e7ef1
David Hernández <david.hernandez@tigears.com>
2018-01-31 00:26:34 -0600

    first commit

---
+++ routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Http\Request;
+
+Route::get('led-on', function() {
+    DB::table('leds')->where('id', '1')->update(['power' => 1]);
+    $led = DB::table('leds')->where('id', '1');
+    return $led->get();
+});
+
+Route::get('led-off', function() {
+    DB::table('leds')->where('id', '1')->update(['power' => 0]);
+    $led = DB::table('leds')->where('id', '1');
+    return $led->get();
+});
+
+Route::get('led-status', function() {
+    $led = DB::table('leds')->where('id', '1')->first();
+    return [
+      'led_status' => $led->power
+    ];
+});
