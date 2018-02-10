0cf6e6c418ce9ee88b2cd5dae20af4f157d85277
David Hernández <david.hernandez@tigears.com>
2018-02-10 13:06:28 -0600

    Response set to json

--- routes/api.php
+++ routes/api.php
@@ -4,14 +4,14 @@ use Illuminate\Http\Request;
 
 Route::get('led-on', function() {
     DB::table('leds')->where('id', '1')->update(['power' => 1]);
-    $led = DB::table('leds')->where('id', '1');
-    return $led->get();
+    $led = DB::table('leds')->where('id', '1')->first();
+    return json_encode($led);
 });
 
 Route::get('led-off', function() {
     DB::table('leds')->where('id', '1')->update(['power' => 0]);
-    $led = DB::table('leds')->where('id', '1');
-    return $led->get();
+    $led = DB::table('leds')->where('id', '1')->first();
+    return json_encode($led);
 });
 
 Route::get('led-status', function() {
