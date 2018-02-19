4bf8ab81b2642be8a34a4d1101a4559c4616e6ab
David Hernández <david.hernandez@tigears.com>
2018-02-19 06:25:24 -0600

    naaa

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -146,7 +146,7 @@ class DeviceController extends Controller
             $plant1->humidity = $request['humidity2'];
             $plant1->save();
         }
-        return response('success', 200);
+        return 'success';
     }
 
 }
